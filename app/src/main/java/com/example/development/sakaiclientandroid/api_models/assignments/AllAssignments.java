7416f2c6b74d0e87e44a3400e6e3c1d82e92b006
SChakravorti21 <shoumyoarchit@gmail.com>
2018-06-09 20:20:41 -0400

    Added POJOs and route for all assignments

---
+++ app/src/main/java/com/example/development/sakaiclientandroid/api_models/assignments/AllAssignments.java
@@ -0,0 +1,42 @@
+package com.example.development.sakaiclientandroid.api_models.assignments;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class AllAssignments implements Serializable
+{
+
+    @SerializedName("entityPrefix")
+    @Expose
+    private String entityPrefix;
+    @SerializedName("assignment_collection")
+    @Expose
+    private List<AssignmentCollection> assignmentCollection = new ArrayList<AssignmentCollection>();
+    private final static long serialVersionUID = 6209006925278274013L;
+
+    public String getEntityPrefix() {
+        return entityPrefix;
+    }
+
+    public void setEntityPrefix(String entityPrefix) {
+        this.entityPrefix = entityPrefix;
+    }
+
+    public List<AssignmentCollection> getAssignmentCollection() {
+        return assignmentCollection;
+    }
+
+    public void setAssignmentCollection(List<AssignmentCollection> assignmentCollection) {
+        this.assignmentCollection = assignmentCollection;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("entityPrefix", entityPrefix).append("assignmentCollection", assignmentCollection).toString();
+    }
+
+}
---
+++ app/src/main/java/com/example/development/sakaiclientandroid/api_models/assignments/SiteOwner.java
@@ -0,0 +1,51 @@
+package com.example.development.sakaiclientandroid.api_models.assignments;
+
+import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class SiteOwner implements Serializable
+{
+
+    @SerializedName("userDisplayName")
+    @Expose
+    private String userDisplayName;
+    @SerializedName("userEntityURL")
+    @Expose
+    private String userEntityURL;
+    @SerializedName("userId")
+    @Expose
+    private String userId;
+    private final static long serialVersionUID = 2203831453507075217L;
+
+    public String getUserDisplayName() {
+        return userDisplayName;
+    }
+
+    public void setUserDisplayName(String userDisplayName) {
+        this.userDisplayName = userDisplayName;
+    }
+
+    public String getUserEntityURL() {
+        return userEntityURL;
+    }
+
+    public void setUserEntityURL(String userEntityURL) {
+        this.userEntityURL = userEntityURL;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("userDisplayName", userDisplayName).append("userEntityURL", userEntityURL).append("userId", userId).toString();
+    }
+
+}
