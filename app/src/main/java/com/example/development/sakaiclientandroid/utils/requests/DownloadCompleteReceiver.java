9fef714e94c2b965e99d72edf9460b4d899ddf7e
SChakravorti21 <shoumyoarchit@gmail.com>
2018-07-08 11:11:02 -0400

    Fixed error with opening files on API 24+

--- app/src/main/java/com/example/development/sakaiclientandroid/utils/requests/DownloadCompleteReceiver.java
+++ app/src/main/java/com/example/development/sakaiclientandroid/utils/requests/DownloadCompleteReceiver.java
@@ -2,15 +2,19 @@ package com.example.development.sakaiclientandroid.utils.requests;
 
 import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.v4.BuildConfig;
 import android.support.v4.content.FileProvider;
 import android.webkit.DownloadListener;
 
 import java.io.File;
 import java.util.HashSet;
+
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
 
 /**
  * Created by Development on 7/7/18.
@@ -30,7 +34,10 @@ public class DownloadCompleteReceiver extends BroadcastReceiver {
         if(downloadId == -1 || !downloads.contains(downloadId))
             return;
 
-        //
+        // We won't be receiving a broadcast for this download again, so remove
+        // the ID from the HashSet
+        downloads.remove(downloadId);
+
         DownloadManager downloadManager =
                 (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
         // We can't get the downloaded file if there is no download manager
@@ -55,10 +62,30 @@ public class DownloadCompleteReceiver extends BroadcastReceiver {
             String uriString = fileCursor.getString(fileUriIndex);
 
             // Get the file and open it if the uri is not empty
-            if(uriString != null && !uriString.trim().equals("")) {
+            if(uriString != null && !uriString.isEmpty()) {
                 Uri fileUri = Uri.parse(uriString);
+
+                // In newer versions of Android (API 24+), the way files are shared
+                // to other applications has changed, and must use the "content://"
+                // scheme with a valid path. FileProvider allows us to expose the file
+                // externally so that it that can be opened by a PDF viewer
+                if(fileUri.getScheme().equals(ContentResolver.SCHEME_FILE)) {
+                    File downloadFile = new File(fileUri.getPath());
+                    fileUri = FileProvider.getUriForFile(context,
+                            String.format("%s.fileprovider", BuildConfig.APPLICATION_ID),
+                            downloadFile);
+                }
+
                 Intent selectViewerIntent = new Intent(Intent.ACTION_VIEW);
                 selectViewerIntent.setData(fileUri);
+                // Flag is necessary for opening application to be able to actually
+                // read the file contents and render it.
+                selectViewerIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                // Allow the user to select what they want to open the file with.
+                // (This is also a failsafe, because if there aren't any apps
+                // that can open the file, Android will provide this message
+                // and our app won't crash if it doesn't find a default app to open with).
                 Intent openIntent = Intent.createChooser(selectViewerIntent,
                         "Select an application to open this file:");
                 context.startActivity(openIntent);
