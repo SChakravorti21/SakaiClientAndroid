58d4700cb2ab72ed09d3e59b8bbcb4822ac0007b
SChakravorti21 <shoumyoarchit@gmail.com>
2018-07-23 22:18:33 -0400

    Added animation to assignment mini cards

--- app/src/main/java/com/example/development/sakaiclientandroid/utils/custom/AssignmentAdapter.java
+++ app/src/main/java/com/example/development/sakaiclientandroid/utils/custom/AssignmentAdapter.java
@@ -11,6 +11,8 @@ import android.transition.Fade;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.example.development.sakaiclientandroid.R;
