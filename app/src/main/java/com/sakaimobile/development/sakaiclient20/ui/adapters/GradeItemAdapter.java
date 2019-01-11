c474034b7bc7feba96a989801e824c5c82d5588f
SChakravorti21 <shoumyoarchit@gmail.com>
2019-01-11 00:46:58 -0500

    Fixed bug with site grades re-using views instead of converting them Also optimized a bunch of code in the GradeItemsAdapter

--- app/src/main/java/com/sakaimobile/development/sakaiclient20/ui/adapters/GradeItemAdapter.java
+++ app/src/main/java/com/sakaimobile/development/sakaiclient20/ui/adapters/GradeItemAdapter.java
@@ -15,22 +15,29 @@ import android.widget.TextView;
 import com.sakaimobile.development.sakaiclient20.R;
 import com.sakaimobile.development.sakaiclient20.persistence.entities.Grade;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GradeItemAdapter extends ArrayAdapter {
 
     private static final float PADDING_LEFT_DP = 10f;
     private static final float PADDING_VERTICAL_DP = 8f;
 
+    private final int PADDING_LEFT_PX;
+    private final int PADDING_VERTICAL_PX;
 
-    private final Context context;
     private final List<Grade> assignmentsList;
+    private Set<Integer> animatedGradePositions;
 
     public GradeItemAdapter(Context context, List<Grade> assignmentsList) {
         super(context, R.layout.gradeitem_node, assignmentsList);
 
-        this.context = context;
         this.assignmentsList = assignmentsList;
+        this.animatedGradePositions = new HashSet<>();
+
+        this.PADDING_LEFT_PX = convertDpToPx(PADDING_LEFT_DP);
+        this.PADDING_VERTICAL_PX = convertDpToPx(PADDING_VERTICAL_DP);
     }
 
 
@@ -41,59 +48,52 @@ public class GradeItemAdapter extends ArrayAdapter {
      * @return the dp in pixels
      */
     private int convertDpToPx(float dp) {
-        int paddingPx = (int) TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                dp,
-                context.getResources().getDisplayMetrics()
+        return (int) TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            getContext().getResources().getDisplayMetrics()
         );
-
-        return paddingPx;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-
-        if (convertView == null) {
-            //create inflater
-            LayoutInflater inflater = (LayoutInflater) context.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
+        Context context = getContext();
+        if(convertView == null) {
             //inflate the row
-            convertView = inflater.inflate(R.layout.gradeitem_node, parent, false);
+            convertView = LayoutInflater.from(context)
+                            .inflate(R.layout.gradeitem_node, parent, false);
+        }
 
-            //get name and grade textviews
-            TextView nameTextView = convertView.findViewById(R.id.txt_assignment_name);
-            TextView gradeTextView = convertView.findViewById(R.id.txt_grade);
+        //get name and grade textviews
+        TextView nameTextView = convertView.findViewById(R.id.txt_assignment_name);
+        TextView gradeTextView = convertView.findViewById(R.id.txt_grade);
 
-            //set the text
-            nameTextView.setText(assignmentsList.get(position).itemName);
+        //set the text
+        Grade assignment = assignmentsList.get(position);
+        String name = assignment.itemName;
+        nameTextView.setText(name);
 
+        String gradeString;
+        //dynamically fills in the string resource
+        if (assignment.grade == null) {
+            gradeString = context.getString(R.string.grade_item_null, assignment.points);
+        } else {
+            gradeString = context.getString(R.string.grade_item, assignment.grade, assignment.points);
+        }
+        gradeTextView.setText(gradeString);
 
-            String gradeString;
-            //dynamically fills in the string resource
-            if (assignmentsList.get(position).grade == null) {
-                gradeString = context.getString(R.string.grade_item_null, assignmentsList.get(position).points);
-            } else {
-                gradeString = context.getString(R.string.grade_item, assignmentsList.get(position).grade, assignmentsList.get(position).points);
-            }
+        //set padding on the name view
+        nameTextView.setPadding(PADDING_LEFT_PX, PADDING_VERTICAL_PX, 0, PADDING_VERTICAL_PX);
 
-            gradeTextView.setText(gradeString);
-
-
-            //set padding on the name view
-            int paddingLeftPx = convertDpToPx(PADDING_LEFT_DP);
-            int paddingVerticalPx = convertDpToPx(PADDING_VERTICAL_DP);
-            nameTextView.setPadding(paddingLeftPx, paddingVerticalPx, 0, paddingVerticalPx);
-
-
+        // Only animate the view if it hasn't been animated before
+        if(!animatedGradePositions.contains(position)) {
             Animation anim = AnimationUtils.loadAnimation(context, R.anim.listview_anim);
-            anim.setStartOffset(position * 100);
+            anim.setStartOffset(100);
             convertView.startAnimation(anim);
-
+            animatedGradePositions.add(position);
         }
 
         return convertView;
-
     }
 }
